0ceed74e37b77f0a469b7dc5e58751abc45ce3c0
Yu-An Ko <handcuffanndo@gmail.com>
2017-06-07 13:14:08 +0800

    create db/seeds folder for seeds files

---
+++ db/seeds/product_seeds.rb
@@ -0,0 +1,11 @@
+# --=== 產品（Product）資料 ===-- #
+
+# 創建產品
+Product.create!(
+  title: "J & A SELECT x Pelikan 聯名限量版 鋼珠筆",
+  description:
+  "J & A SELECT 貫徹「講究生活裡細微的美好」的選物職人精神，為你搜羅來自歐美、日本、台灣等地的夢幻逸品，用文具改變你的桌上風景。",
+  price: 270,
+  quantity: 4
+)
+puts "創建商品 * 1"
